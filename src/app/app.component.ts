e53b3bc1da222dcdac03dbc488b98fcd3ac0184e
BELGHITI <nabil.belghiti@gmail.com>
2017-10-17 10:58:44 +0200

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,30 +5,48 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { MapPage } from '../pages/map/map';
+import { UserPage } from '../pages/user/user';
 
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
+export class SO_extreme {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+    rootPage: any;
+  // fonction connexion authomatique si l'utilisateur est deja connecté et si il demander l'enregistrement de ces identifiant
+  // verifier la disponibilité des IDs dans localStorage
+  // definir la page de demarage si oui HomePage si nn LoginPage
+  checkPreviousAuthorization(): void { 
+    if((window.localStorage.getItem('email') === "undefined" || window.localStorage.getItem('email') === null) && 
+       (window.localStorage.getItem('password') === "undefined" || window.localStorage.getItem('password') === null)) {
+      this.rootPage = LoginPage;
+    console.log("LoginPage");
+    } else {
+      this.rootPage = HomePage;
+    }
+  }
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{title: any, component: any, icon: any}>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+    this.pages =  [
+      { title: 'Home Page', component: HomePage, icon: 'ios-home-outline' },
+      { title: 'Ma List ', component: ListPage, icon: 'ios-clipboard-outline' },
+      { title: 'Ma Position', component: MapPage, icon: 'ios-map-outline' },
+      { title: 'Users', component: UserPage, icon: 'ios-people-outline' }
     ];
 
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      this.checkPreviousAuthorization();
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,33 +2,58 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
-import { MyApp } from './app.component';
+import { SO_extreme } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { MapPage } from '../pages/map/map';
+import { UserPage } from '../pages/user/user';
+import { RegisterPage } from '../pages/register/register';
 
+import { Geolocation } from '@ionic-native/geolocation';
+import { HttpModule  } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Rest } from '../providers/rest/rest';
+import { AuthService } from '../providers/auth-service/auth-service';
+import { SafePipe } from '../pipes/safe/safe';
+
 
 @NgModule({
   declarations: [
-    MyApp,
+    SO_extreme,
     HomePage,
-    ListPage
+    ListPage,
+    LoginPage,
+    UserPage,
+    MapPage,
+    RegisterPage,
+    SafePipe
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    HttpModule ,
+    HttpClientModule,
+    IonicModule.forRoot(SO_extreme)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
+    SO_extreme,
     HomePage,
-    ListPage
+    ListPage,
+    LoginPage,
+    UserPage,
+    MapPage,
+    RegisterPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Geolocation,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Rest,
+    AuthService 
   ]
 })
 export class AppModule {}
